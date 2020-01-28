e54c9fe257d6c6acb402be86df41a98c5d14d9bf
eliasmagnusson12 <EXT_BVB@qlik.com>
2020-01-28 12:18:54 +0100

    newFetch

--- work/report/js/fetchSchools.js
+++ work/report/js/fetchSchools.js
@@ -8,11 +8,9 @@
         })
         .then((myJson) => {
             console.log(myJson);
-        });
+        });  
 
-       var el_up = document.getElementById("1081.json");
-       var list = el_up
-
+        
         
         // 1. Create the button
         var button = document.createElement("button");
@@ -24,58 +22,11 @@
 
         // 3. Add event handler
         button.addEventListener ("click", function() {
-            GFG_FUN()
+            
         alert("did something");
         });
 
-        function GFG_FUN() { 
-            var cols = []; 
-              
-            for (var i = 0; i < list.length; i++) { 
-                for (var k in list[i]) { 
-                    if (cols.indexOf(k) === -1) { 
-                          
-                        // Push all keys to the array 
-                        cols.push(k); 
-                    } 
-                } 
-            } 
-              
-            // Create a table element 
-            var table = document.createElement("table"); 
-              
-            // Create table row tr element of a table 
-            var tr = table.insertRow(-1); 
-              
-            for (var i = 0; i < cols.length; i++) { 
-                  
-                // Create the table header th element 
-                var theader = document.createElement("th"); 
-                theader.innerHTML = cols[i]; 
-                  
-                // Append columnName to the table row 
-                tr.appendChild(theader); 
-            } 
-              
-            // Adding the data to the table 
-            for (var i = 0; i < list.length; i++) { 
-                  
-                // Create a new row 
-                trow = table.insertRow(-1); 
-                for (var j = 0; j < cols.length; j++) { 
-                    var cell = trow.insertCell(-1); 
-                      
-                    // Inserting the cell at particular place 
-                    cell.innerHTML = list[i][cols[j]]; 
-                } 
-            } 
-              
-            // Add the newely created table containing json data 
-            var el = document.getElementById("table"); 
-            el.innerHTML = ""; 
-            el.appendChild(table); 
-        }    
-
+      
 
         
 
