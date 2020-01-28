522c0772f6b5775d06bd4c6188cd6ad6d95c100e
eliasmagnusson12 <EXT_BVB@qlik.com>
2020-01-28 10:29:14 +0100

    Event on button

--- work/report/js/fetchSchools.js
+++ work/report/js/fetchSchools.js
@@ -10,9 +10,13 @@
             console.log(myJson);
         });
 
+       var el_up = document.getElementById("1081.json");
+       var list = el_up
+
+        
         // 1. Create the button
         var button = document.createElement("button");
-        button.innerHTML = "Do Something";
+        button.innerHTML = "Fetch Schools";
 
         // 2. Append somewhere
         var body = document.getElementsByTagName("body")[0];
@@ -20,11 +24,60 @@
 
         // 3. Add event handler
         button.addEventListener ("click", function() {
+            GFG_FUN()
         alert("did something");
         });
 
+        function GFG_FUN() { 
+            var cols = []; 
+              
+            for (var i = 0; i < list.length; i++) { 
+                for (var k in list[i]) { 
+                    if (cols.indexOf(k) === -1) { 
+                          
+                        // Push all keys to the array 
+                        cols.push(k); 
+                    } 
+                } 
+            } 
+              
+            // Create a table element 
+            var table = document.createElement("table"); 
+              
+            // Create table row tr element of a table 
+            var tr = table.insertRow(-1); 
+              
+            for (var i = 0; i < cols.length; i++) { 
+                  
+                // Create the table header th element 
+                var theader = document.createElement("th"); 
+                theader.innerHTML = cols[i]; 
+                  
+                // Append columnName to the table row 
+                tr.appendChild(theader); 
+            } 
+              
+            // Adding the data to the table 
+            for (var i = 0; i < list.length; i++) { 
+                  
+                // Create a new row 
+                trow = table.insertRow(-1); 
+                for (var j = 0; j < cols.length; j++) { 
+                    var cell = trow.insertCell(-1); 
+                      
+                    // Inserting the cell at particular place 
+                    cell.innerHTML = list[i][cols[j]]; 
+                } 
+            } 
+              
+            // Add the newely created table containing json data 
+            var el = document.getElementById("table"); 
+            el.innerHTML = ""; 
+            el.appendChild(table); 
+        }    
 
-        var x = document.getElementById("kommun")
+
+        
 
     console.log('Sandbox MEGA is ready!');
 })();
