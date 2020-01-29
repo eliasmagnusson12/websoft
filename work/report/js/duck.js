3fa9e2b1cdece7369d54705166541262ce7e3bbe
Elias Magnusson <magnusson.elias1@gmail.com>
2020-01-29 14:35:56 +0100

    show/hide duck

--- work/report/js/duck.js
+++ work/report/js/duck.js
@@ -4,6 +4,13 @@
 
     var element = document.getElementById("duck");
 
+    var hidden = false;
+
+setInterval(function(){
+    document.getElementById("duck").style.visibility= hidden ? "visible" : "hidden";
+    hidden = !hidden;
+},2000);
+
     element.addEventListener("click", function(){
         
         element.style.left = element.offsetLeft + 20 + "px";
