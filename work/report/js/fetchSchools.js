9b4055858e6f53f371b707128ba2b438be4f076a
eliasmagnusson12 <EXT_BVB@qlik.com>
2020-01-30 10:26:08 +0100

    dropDownUpdate

--- work/report/js/fetchSchools.js
+++ work/report/js/fetchSchools.js
@@ -52,22 +52,32 @@ fetch('https://api.scb.se/UF0109/v2/skolenhetsregister/sv/kommun/1282')
         });  
 
         
-         document.getElementById("myDropdown").classList.toggle("show");
-          
-          
-          // Close the dropdown menu if the user clicks outside of it
-          window.onclick = function(event) {
-            if (!event.target.matches('.dropbtn')) {
-              var dropdowns = document.getElementsByClassName("dropdown-content");
-              var i;
-              for (i = 0; i < dropdowns.length; i++) {
-                var openDropdown = dropdowns[i];
-                if (openDropdown.classList.contains('show')) {
-                  openDropdown.classList.remove('show');
-                }
-              }
-            }
-          }
+        var button1 = getElementById('dropbtn');
+        button.innerHTML = "Select School";
+
+        button1.addEventListener ("click", function() {
+            myFunction();
+        
+        });
+        
+        
+        function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
 
 
         
