6cc1cfef10c7c009c7de24d5352b5afdc352633e
eliasmagnusson12 <EXT_BVB@qlik.com>
2020-01-30 10:11:31 +0100

    dropdown.2

--- work/report/js/fetchSchools.js
+++ work/report/js/fetchSchools.js
@@ -51,6 +51,24 @@ fetch('https://api.scb.se/UF0109/v2/skolenhetsregister/sv/kommun/1282')
             
         });  
 
+        
+         document.getElementById("myDropdown").classList.toggle("show");
+          
+          
+          // Close the dropdown menu if the user clicks outside of it
+          window.onclick = function(event) {
+            if (!event.target.matches('.dropbtn')) {
+              var dropdowns = document.getElementsByClassName("dropdown-content");
+              var i;
+              for (i = 0; i < dropdowns.length; i++) {
+                var openDropdown = dropdowns[i];
+                if (openDropdown.classList.contains('show')) {
+                  openDropdown.classList.remove('show');
+                }
+              }
+            }
+          }
+
 
         
 
